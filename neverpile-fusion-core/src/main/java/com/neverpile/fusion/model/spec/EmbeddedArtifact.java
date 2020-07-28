3c39389729e8e6e20fffc01bfd4fa31bf50499d2
Jörg Henne <j.henne@levigo.de>
2020-07-28 17:21:15 +0200

    feat: Add specification type EmbeddedArtifact (fix #15)

---
+++ neverpile-fusion-core/src/main/java/com/neverpile/fusion/model/spec/EmbeddedArtifact.java
@@ -0,0 +1,38 @@
+package com.neverpile.fusion.model.spec;
+
+import org.springframework.http.MediaType;
+
+/**
+ * A general artifact like {@link Artifact} which has its content embedded in the collection instead
+ * of being referenced from an external source. This should generally only be used for small payload
+ * sizes up to a few kilobytes.
+ */
+public class EmbeddedArtifact extends Specification {
+  /**
+   * The media type of the artifact. Used, e.g. by frontend applications to find a suitable viewer.
+   */
+  private MediaType mediaType;
+
+  /**
+   * The actual artifact content. The content may be, for example, encoded as Base64. This must be
+   * reflected in the media type by adding an appropriate parameter, e.g.
+   * <code>type/subtype;base64</code>.
+   */
+  private String content;
+
+  public MediaType getMediaType() {
+    return mediaType;
+  }
+
+  public void setMediaType(final MediaType mediaType) {
+    this.mediaType = mediaType;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(final String content) {
+    this.content = content;
+  }
+}
