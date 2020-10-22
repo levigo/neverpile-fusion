18d444edc9fff00961debf607c6e47e517bfc58f
Jörg Henne <j.henne@levigo.de>
2020-10-22 19:00:31 +0200

    bug: Fix #31 - string set converter corner cases

--- neverpile-fusion-jpa/src/main/java/com/neverpile/fusion/jpa/SemicolonDelimitedStringSetConverter.java
+++ neverpile-fusion-jpa/src/main/java/com/neverpile/fusion/jpa/SemicolonDelimitedStringSetConverter.java
@@ -1,5 +1,6 @@
 package com.neverpile.fusion.jpa;
 
+import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -13,17 +14,27 @@ public class SemicolonDelimitedStringSetConverter implements AttributeConverter<
 
   @Override
   public String convertToDatabaseColumn(final Set<String> stringList) {
-    if (stringList == null) {
+    if (stringList == null || stringList.isEmpty()) {
       return "";
     }
-
-    return stringList.stream() //
+    
+    String text = stringList.stream() //
         .map(s -> s.replace(DELIMITER, "\\" + DELIMITER)) //
-        .collect(Collectors.joining(DELIMITER));
+        .collect(Collectors.joining(DELIMITER, "", DELIMITER));
+    return text;
   }
 
   @Override
   public Set<String> convertToEntityAttribute(final String string) {
+    if(string.isEmpty()) {
+      return Collections.emptySet();
+    }
+    
+    // due to the way split() below works, we need one special case treatment for a single, empty string
+    if(string.equals(DELIMITER)) {
+      return Collections.singleton("");
+    }
+    
     return Stream //
         .of(string.split("(?<!\\\\)" + DELIMITER)) //
         .map(s -> s.replace("\\" + DELIMITER, DELIMITER)) //
